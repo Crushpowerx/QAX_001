941c9997694c047c61689e9622b71e99334f93e5
crushpowerx <asovin.e.v@gmail.com>
2021-03-28 12:42:11 +0300

    upd

---
+++ src/test/java/tests/AbstractClassExample.java
@@ -0,0 +1,11 @@
+package tests;
+
+abstract class AbstractClassExample {
+
+    abstract int getSum(int var1, int var2);
+
+    public void printSum(int var1, int var2) {
+        System.out.println(var1 + var2);
+    }
+
+}
