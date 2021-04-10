98e8447aec1c9ad179b55c0f3ef3f83f2e30bbcb
crushpowerx <asovin.e.v@gmail.com>
2021-04-10 11:18:23 +0300

    upd

---
+++ src/test/java/tests/testng/TestngExample.java
@@ -0,0 +1,12 @@
+package tests.testng;
+
+import org.testng.annotations.Test;
+
+public class TestngExample {
+
+    @Test
+    public void test1() {
+        System.out.println("I am test1");
+    }
+
+}
