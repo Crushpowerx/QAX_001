41d01120f1df5714f77845deed2499f100398c21
Crushpowerx <asovin.e.v@gmail.com>
2021-04-25 13:03:42 +0300

    upd

--- src/test/java/pages/google/actions/GoogleMainPage.java
+++ src/test/java/pages/google/actions/GoogleMainPage.java
@@ -1,6 +1,7 @@
 package pages.google.actions;
 
 import io.qameta.allure.Step;
+import logger.CustomLogger;
 import pages.google.locators.GoogleMainPageLocators;
 
 public class GoogleMainPage extends GoogleMainPageLocators {
@@ -8,11 +9,13 @@ public class GoogleMainPage extends GoogleMainPageLocators {
     @Step("Fill the field search")
     public void fillFieldSearch(String value) {
         fieldSearch.sendKeys(value);
+        CustomLogger.logger.info(value + " - ok");
     }
 
     @Step("Click the button search")
     public void clickButtonSearch() {
         buttonSearch.click();
+        CustomLogger.logger.info("ok");
     }
 
 }
