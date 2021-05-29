921eb74b866c531bff7c1b432396245f5a91bd76
Crushpowerx <asovin.e.v@gmail.com>
2021-05-29 10:22:40 +0300

    upd

--- src/test/java/global/Global.java
+++ src/test/java/global/Global.java
@@ -1,11 +1,11 @@
 package global;
 
 public class Global {
+    public static boolean globalModeDebug = false;
     public static String globalCountry = "";
     public static String globalRandomLetters = "";
     public static String globalDevice = "";
     public static boolean globalIsMobileDevice = false;
-    public static boolean globalModeDebug = false;
 
     public static void resetGlobalVariablesAfterClass() {
         globalCountry = "";
